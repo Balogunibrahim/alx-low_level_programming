6cf4a2c7a7238560dc6fcb51ce31528138cfc753
Balogun Ibrahim <balooopy30@gmail.com>
2023-02-19 15:14:34 -0800

    printing a-z in lower case

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,12 @@
  */
 int main(void)
 {
-	int c;
+	int ch;
 
-	for (c = "a"; c <= "z"; c++)
+	for (ch ='a'; ch <= 'z'; ch++)
 	{
-		putchar(c);
+		putchar(ch);
 	}
-	putchar("\n");
-return (0);
+	putchar('\n');
+	return (0);
 }
-
