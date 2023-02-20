018b3379213b5c45b305555468b233365be82370
Balogun Ibrahim <balooopy30@gmail.com>
2023-02-19 16:20:40 -0800

    Printing 0-9 using the putchar function

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: Print 0-9 using the putchar function
+ * Return: o
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + "0");
+	}
+	putchar('\n');
+	return (0);
+}
