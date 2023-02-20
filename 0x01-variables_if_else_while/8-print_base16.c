af9af8e6218b07d9f274a0a5c9115789fb2e653d
Balogun Ibrahim <balooopy30@gmail.com>
2023-02-19 16:50:12 -0800

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ *  main - Entry point
+ *  Description: prints every singe digit in Base 16
+ *  Return: 0
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	
+	for (i = 0; i < 6; i++)
+	{
+		putchar(i + 'a');
+	}
+
+	putchar('\n');
+	return (0);
+}
