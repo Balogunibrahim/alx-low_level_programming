3df264a911aecbc636bbf901c3b1392b36c80d68
Balogun Ibrahim <balooopy30@gmail.com>
2023-02-22 15:06:19 -0800

    a function that checks and retruns 1 if it is a letter, uppercase or lowercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha- a function that checks if a character is upper case
+ *
+ * @c: The character that is checked
+ *
+ * Return: 1 if it the character is uppercase and 0 if it is otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z')
+		return (1);
+	else 
+		return (0);
+}
