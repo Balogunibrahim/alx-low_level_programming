2135fceb22bb26222064fb242b5cfcdb7264dfac
Balogun Ibrahim <balooopy30@gmail.com>
2023-02-22 15:28:17 -0800

    a function that checks and retruns 1 if it is a letter, uppercase or lowercase

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_sign- prints the sign of a number
+ *
+ * @n: The character that is checked
+ *
+ * Return: returns 1 if the number is positive,
+ *
+ * 0 if the number is equal to zero,
+ *
+ * -1 if the number is negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
