0ee65d9460ac3f3ec2368fb89f0e01d5510f5a73
Balogun Ibrahim <balooopy30@gmail.com>
2023-02-22 16:00:39 -0800

    a function that returns the absolute value of n

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - Prints the absolute value of n
+ *
+ * @n:The value that is checked
+ *
+ * Return: returns n and -n if n is negative
+ */
+int _abs(int)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
