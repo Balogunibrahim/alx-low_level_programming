254a92d6d6a1c158baae79c4a8966b057c6610fc
Balogun Ibrahim <balooopy30@gmail.com>
2023-02-22 17:26:33 -0800

    add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - Adds two interger
+ *
+ * @n1: The first interger
+ *
+ * @n2: The second interger
+ *
+ * Return: The result of the addition
+ *
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
