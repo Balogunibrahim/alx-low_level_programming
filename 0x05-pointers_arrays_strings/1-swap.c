7466b7bbe84c3dce80acba43f7a4b1b67cfe4b39
Balogun Ibrahim <balooopy30@gmail.com>
2023-02-27 15:06:57 -0800

    a function that swaps to interger

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * swap_int - a funtion that swaps two interger usingpointers
+ *
+ * @a: first int
+ *
+ * @b: second int
+ *
+ * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	int s;
+	s = *a;
+	*a = *b;
+	*b = s;
+}
