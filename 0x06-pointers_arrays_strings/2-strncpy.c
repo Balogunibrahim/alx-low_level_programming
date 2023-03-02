89f609cb7f5be334bc5c83dcb4e160569e953b5e
Balogun Ibrahim <balooopy30@gmail.com>
2023-03-02 03:40:40 -0800

    a function that copies a string and append to a destination

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncpy- Afunction that copies a string from src to dest
+ *
+ * @dest: The character that is checked
+ *
+ * @src: The character that is checked
+ *
+ * @n: The character that is checked
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *d = dest;
+	char *s = src;
+	int n;
+
+	for (i = 0; i < n && *s != '\0'; i++)
+	{
+		*d++ = *s++;
+	}
+	for (; i < n; i++)
+	{
+		*d++ = '\0';
+	}
+	return (dest);
+}
