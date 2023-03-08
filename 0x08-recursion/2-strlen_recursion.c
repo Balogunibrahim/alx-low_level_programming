060d1d4e744a35aca1ca8941c08eea7befaabce3
Balogun Ibrahim <balooopy30@gmail.com>
2023-03-08 15:35:04 -0800

    a function that returns the length of a string.

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /*
- * _strlen_recursion - returns the length of a string.
+ * _strlen_recursion - returns the length of a string using recursion.
  * @s: The character that is checked
  * Return: The length of the string
  */
