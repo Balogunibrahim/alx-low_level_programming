3ccc9bd8cc2e6e3c258069c9f8d14ce557dda96b
Balogun Ibrahim <balooopy30@gmail.com>
2023-03-21 06:20:01 -0700

    an header file that has the attribute of a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _D0G_H_
+#define _DOG_H_
+/**
+ * struct dog - Information about a dog
+ * @name: name of the dog
+ * @age: the age of the dog
+ * @owner: information about the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif /*_DOG_H_*/
