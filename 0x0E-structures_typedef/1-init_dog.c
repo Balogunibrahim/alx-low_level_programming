d846fe7439288133e069d12aa610b8d1bc2a0cb4
Balogun Ibrahim <balooopy30@gmail.com>
2023-03-21 10:46:54 -0700

    initializing variables for the structure dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,7 +7,7 @@
  * @owner: owner of the dog
  * Return: void
  */
-void init_dog(struct dog *d, char *name, float *age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if(d)
 	{
