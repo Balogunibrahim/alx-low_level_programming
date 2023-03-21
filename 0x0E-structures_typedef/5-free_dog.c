61953946a5f54f544d4b0ef13d2b9e88b0958951
Balogun Ibrahim <balooopy30@gmail.com>
2023-03-21 14:17:28 -0700

    a fucntion that free dog struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include "dog.h"
+/**
+ * free_dog- a function that free struct dog
+ * @d: the variable that points to struct dog
+ * Return: void
+ */
+void free_dog (dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
