2827bda7c2cd9a3e87045a3b21c391672d24fb13
Balogun Ibrahim <balooopy30@gmail.com>
2023-03-23 18:26:52 -0700

    variadic function that prints all data type

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,56 +1,51 @@
-#include<stdio.h>
-#include<stdarg.h>
-#include "variadic_function.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
 /**
- * print_all- a variadic that prints mutiple data type
- * @format: the format to follow
- * Return: 0
- */
+  *print_all - prints anything.
+  *@format: list of all arguments passed to the function.
+  *
+  *Return: void.
+  */
 void print_all(const char * const format, ...)
 {
-	va_list(all);
-	char c;
-	int i;
-	float f;
-	char *s;
+	unsigned int i;
+	va_list args;
+	char *s, *separator;
 
-	va_start(all, format);
+	va_start(args, format);
 
-	while (*format)
+	separator = "";
+
+	i = 0;
+	while (format && format[i])
 	{
-		if (*format == "c")
+		switch (format[i])
 		{
-			c = va_arg(all, int);
-			printf("%c", c);
+			case 'c':
+				printf("%s%c", separator,  va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(args, double));
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				if (s == NULL)
+					s = "(nil)";
+				printf("%s%s", separator, s);
+				break;
+			default:
+				i++;
+				continue;
 		}
-		else if (*format == "i")
-		{
-			i = va_arg(all, int);
-			printf("%d", i);
-		}
-		else if (*format == "f")
-		{
-			f = va_arg(all, double);
-			printf("%f", f);
-		}
-		else if (*format == "s")
-		{
-			s = va_arg(all, char *);
-			if (s == NULL)
-			{
-				printf("(nil)");
-			}
-			else if
-			{
-				printf("%s", s);
-			}
-		}
-		format++;
-		if (*format)
-		{
-			printf(", ");
-		}
+		separator = ", ";
+		i++;
 	}
+
 	printf("\n");
-	va_end(all);
+	va_end(args);
 }
