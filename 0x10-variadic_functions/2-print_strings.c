847bd028ea91bdbe50b9bcd4b39bf011f4510446
Balogun Ibrahim <balooopy30@gmail.com>
2023-03-23 17:18:08 -0700

    a variadic function that prints a set of strings with a seperator

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_strings- a function that print set of strings with a seperator
+ * @seperator: the seperator
+ * @n: the size of the argument
+ */
+void print_strings(const char *seperator, const unsigned int n, ...)
+{
+	va_list(strings);
+	unsigned int i;
+	const char *s;
+
+	va_start(strings, n);
+
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(strings, const char *);
+		if (s == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", s);
+		}
+		
+		if(i < (n - 1) && seperator != NULL)
+		{
+			printf("%s", seperator);
+		}
+	}
+	printf("\n");
+	va_end(strings);
+}
