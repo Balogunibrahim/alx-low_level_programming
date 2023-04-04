55b13fe20dfb7f1a29ea4f3e2bed26b50459f753
Balogun Ibrahim <balooopy30@gmail.com>
2023-04-03 17:47:23 -0700

    a function that prints a linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_listint- a function that prints a singy linked list
+ * @h: the list to be printed
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
+
