123d5eeed9e39ad7f5aa15efe19deb2afc571865
Balogun Ibrahim <balooopy30@gmail.com>
2023-04-03 17:58:52 -0700

    a function that returns the size of a linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * listint_len- a function that returns te number of a node
+ * @h: the parameter to be given
+ *
+ * Return: the size of a list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->n;
+	}
+	return (count);
+}
